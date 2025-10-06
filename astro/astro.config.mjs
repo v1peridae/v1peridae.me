ab0fc8cbc875b4812bc51f7a4ade3ba2f4073759
louisa :3 <louisakavindu@gmail.com>
2025-10-06 22:51:09 +0300

    added about + started working on projects page

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,7 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true, 
+  },
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      host: true,
+      allowedHosts: [
+        "uncontortive-addie-unenriched.ngrok-free.dev",
+        "localhost",
+        "127.0.0.1"
+      ]
+    },
   },
 });
